5adddc2bf7e720b710cec79d507dfaabbd42da97
veektrie <102025110+veektrie@users.noreply.github.com>
2022-05-17 22:18:56 +0100

    home and nav bar added

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className="Navbar">
+            <h1>
+                The Dojo Blog
+            </h1>
+            <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
