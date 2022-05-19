7a56ede9e700e944648343446fe5f4b9ed231026
veektrie <102025110+veektrie@users.noreply.github.com>
2022-05-19 07:53:28 +0100

    click events

--- src/home.js
+++ src/home.js
@@ -1,13 +1,19 @@
 const Home = () => {
-    const handleClick = () => {
-        console.log('hello , ninjas');
+
+    const handleClickAgain = (e) => {
+        console.log( 'hello, ninjas',e );
     }
+    const handleClick = (name , e) => {
+        console.log( 'hello' + (name), e.target);
+    }
+    
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick={handleClick}> Click Me</button>
+            <button onClick={handleClickAgain}> click again </button>
+            <button onClick={(e) =>  handleClick(' yoshi',e )}> Click Me</button>
         </div>
      );
-}
+ }
  
 export default Home;
