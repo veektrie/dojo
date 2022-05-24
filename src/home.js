cf409cf9b0a5714f523feb863ad9fee30ac8ef39
veektrie <102025110+veektrie@users.noreply.github.com>
2022-05-24 20:32:03 +0100

    function as props

--- src/home.js
+++ src/home.js
@@ -5,12 +5,17 @@ const Home = () => {
     const [ blogs , setBlogs] = useState([
         {title:'my new website', body:'lorem ipsum...', author:'mario', id:1},
         {title:'welcome party', body:'lorem ipsum...', author:'yondu', id:2},
-        {title:'web dev top tips', body:'lorem ipsum...', author:'yarny', id:3}
+        {title:'web dev top tips', body:'lorem ipsum...', author:'yarny', id:3},
+        {title:'dev top tips', body:'lorem ipsum...', author:'yarny', id:4}
     ]);
+    const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !== id);
+      setBlogs(newBlogs);  
+    }
 
      return ( 
         <div className="home">
-          <Bloglist blogs={blogs} title="All Blogs!"/>
+          <Bloglist blogs={blogs} title="All Blogs!" handleDelete = {handleDelete}/>
           <Bloglist blogs={blogs.filter((blog)=> blog.author === 'yarny') } title="Yarny blogs"/>
 
         </div>
